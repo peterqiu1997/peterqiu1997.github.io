a5f821dc37ad52638dc737c1e331f4876cbdb8f4
Peter Qiu <peterqiu1997@gmail.com>
2016-11-14 20:03:36 -0800

    first

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="content">
+        <h1>hello</h1>
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+const app = document.getElementById("app_root");
+
+ReactDOM.render(<App />, app);
