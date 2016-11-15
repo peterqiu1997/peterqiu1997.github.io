b5a159946edc55d58ab20ab7f8e2bbdc10a5caf9
Peter Qiu <peterqiu1997@gmail.com>
2016-11-14 20:37:44 -0800

    moved some files

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.join(__dirname, "/docs"),
+    path: path.join(__dirname, ""),
     filename: "index.min.js"
   },
   plugins: debug ? [] : [
