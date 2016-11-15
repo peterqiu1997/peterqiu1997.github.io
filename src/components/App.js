53bcf43a021d5a049cf2534ea30c4511f5f439be
Peter Qiu <peterqiu1997@gmail.com>
2016-11-14 20:32:02 -0800

    site

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+require('../styles/base.scss');
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -8,7 +10,10 @@ export default class App extends React.Component {
   render() {
     return (
       <div class="content">
-        <h1>hello</h1>
+        <h1>hello this will not be my website</h1>
+        <h1>this is a picture of me </h1>
+        <img src={"./profile.jpg"}/>
+        <h2>i love coding</h2>
       </div>
     );
   }
