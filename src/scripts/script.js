7ce52832246c0ed2803b3052f28e8471445ef0db
Peter Qiu <peterqiu1997@gmail.com>
2017-01-16 01:42:53 -0800

    small change

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,7 +2,7 @@ var descriptions = [
     ["a maker", "of better descriptions than maker."],
     ["a percussive fingerstyle guitarist", "a mouthful."],
     ["a cook", "in the loosest sense of the word."],
-    ["a tinkerer", "break it to make it."],
+    ["a tinkerer", "break it and make it."],
     ["a player", "of the instrumental type."],
     ["a designer", "putting user experience first."],
     ["a programmer", "playing the keyboard."]
