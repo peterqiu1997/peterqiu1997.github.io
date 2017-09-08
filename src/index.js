47f91b790ad24768dd17d70caa9d052369baddfa
Peter Qiu <peterqiu1997@gmail.com>
2017-09-07 23:47:46 -0700

    initial

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.js";
-
-const app = document.getElementById("app_root");
-
-ReactDOM.render(<App />, app);
